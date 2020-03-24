73cbb86b779794d0ad09602c801c232026ed5edc
muhtar139 <muhtar_ola87@mail.ru>
2020-03-24 20:11:54 +0600

    для слияние с удаленном

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="описание сайта">
+    <meta name="keywords" content="ключевые слова">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+    <script language="javascript" scr="javasrcipts.js"></script>
+    <title>тестовая страница</title>
+</head>
+<body>
+<form action="login.php" method="POST">
+<div class="container">
+    <img src="img/car.jpg">
+    <form>
+        <div class="dws-input">
+            <input type="text" name="username" placeholder="Введите логин">
+        </div>
+        <div class="dws-input">
+            <input type="password" name="password" placeholder="Введите пароль">
+        </div>
+        <input class="dws-submit" type="submit" name="submit" value="ВОЙТИ">
+        </br>
+        <a href="#">Восстановить пароль</a>
+    </form>
+    <div class="social">
+        <i class="fa fa-vk" aria-hidden="true"></i>
+        <i class="fa fa-odnoklassniki" aria-hidden="true"></i>
+        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+        <i class="fa fa-google-plus" aria-hidden="true"></i>
+        <i class="fa fa-pinterest" aria-hidden="true"></i>
+    </div>
+</div>
+</form>
+</body>
+</html>
